436116957b92d4225d60f0a474183bc4bd8209a5
philip82148 <92205270+philip82148@users.noreply.github.com>
2025-05-27 10:52:54 +0900

    set up for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -9,6 +11,7 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  base: "/repository-name/",
   plugins: [
     remix({
       future: {
@@ -17,6 +20,16 @@ export default defineConfig({
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+      },
+      ssr: false,
+      basename: "/repository-name/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
       },
     }),
     tsconfigPaths(),
